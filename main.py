575f746ae2ff68d67f20fffc2218aeb2527d73a3
unknown <tanguy.pledel@gmail.com>
2023-04-15 18:56:56 +0200

    add create a csv file to store data feature

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import csv
+
 import requests
 from bs4 import BeautifulSoup
 
-# Type GET request
-r = requests.get('https://www.welcometothejungle.com/fr/companies/datascientest/jobs/developpeur-python-h-f_puteaux?q=d3210e5fd6fc0fca57024e5ac29d0164&o=1647471')
+# Parameters
+offer_link = 'https://www.welcometothejungle.com/fr/companies/datascientest/jobs/developpeur-python-h-f_puteaux?q=d3210e5fd6fc0fca57024e5ac29d0164&o=1647471'
 
+# Type GET request
+r = requests.get(offer_link)
 # Storing the content of the response
 webpage_html = r.text
 
@@ -11,33 +15,31 @@ webpage_html = r.text
 soup = BeautifulSoup(webpage_html, "lxml")
 
 ### Accessing different elements on the page
+job_title = soup.find('h1').text
+contract_type = soup.find('i', {"name":"contract"}).parent.parent.text
+job_location = soup.find('i', {"name":"location"}).parent.parent.text
+remote_work = soup.find('i', {"name":"remote"}).parent.parent.text
+education_level = soup.find('i', {"name":"education_level"}).parent.parent.text
+experience_needed = soup.find('i', {"name":"suitcase"}).parent.parent.text
+sector = soup.find('i', {"name":"tag"}).parent.parent.text
+n_employees = soup.find('i', {"name":"department"}).parent.parent.text
+company_name = soup.find('h3', {'data-testid':'job-header-organization-title'}).text
+company_link = "https://www.welcometothejungle.com" + soup.find('a', {"data-testid":"job-header-organization-link-logo"}).get('href')
+offer_text = soup.find("section", {"id":"about-section"}).text.replace('\n', ' ')
 
-# Contract type
-print("Type de contrat :", soup.find('i', {"name":"contract"}).parent.parent.text)
+# Organizing columns names in a list 
+column_names = ["Titre", 'Contrat', 'Localisation', 'Télétravail', 'Etudes', 'Expérience', 'Entreprise', 'Domaine', 'Employés', 'Lien_entreprise', 'Lien_offre', 'Offre' ]
+# Organizing variables in a list
+job_data = [job_title, contract_type, job_location, remote_work, education_level, experience_needed,  company_name, sector, n_employees, company_link, offer_link, offer_text]
 
-# Location
-print("Job Location:", soup.find('i', {"name":"location"}).parent.parent.text)
+# Opening a file
+with open('job_offers.csv', 'a', encoding='utf-8', newline='') as csv_file:
+    # Create a writer object
+    writer = csv.writer(csv_file)
+    # Add a line with columns 
+    if csv_file.tell() == 0:
+        writer.writerow(column_names)
+    # Add data for the offer
+    writer.writerow(job_data)
 
-# Education level
-print("Education Level:", soup.find('i', {"name":"education_level"}).parent.parent.text)
-
-# Remote work
-print("Remote possibility:", soup.find('i', {"name":"remote"}).parent.parent.text)
-
-# Experience
-print("Experience needed:", soup.find('i', {"name":"suitcase"}).parent.parent.text)
-
-# Sector
-print("Domaine:", soup.find('i', {"name":"tag"}).parent.parent.text)
-
-# Number of collaborators
-print("Collaborators:", soup.find('i', {"name":"department"}).parent.parent.text)
-
-# Company name
-print("\nCompagny Name:", soup.find('h3', {'data-testid':'job-header-organization-title'}).text)
-
-# Company link
-print("Company Link :", soup.find('a', {"data-testid":"job-header-organization-link-logo"}).get('href'))
-
-# Job offer text
-print(soup.find("section", {"id":"about-section"}).text)
+    # Close automatically the file
